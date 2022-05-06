59b71778d706b47d02db21c9d171d720b1bb80b3
Pratik Banerjee <pratbanerjee@deloitte.com>
2022-05-06 14:51:17 +0530

    Initial commit

---
+++ src/main/java/com/deloitte/missiongraph/mgserializer/MgSerializerApplication.java
@@ -0,0 +1,13 @@
+package com.deloitte.missiongraph.mgserializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgSerializerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgSerializerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deloitte/missiongraph/mgserializer/MgSerializerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deloitte.missiongraph.mgserializer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgSerializerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
